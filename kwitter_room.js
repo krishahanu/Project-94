eebaa6d648bd10ea220f223b1ec7510662b3ad32
krishahanu <127421687+krishahanu@users.noreply.github.com>
2023-04-14 13:03:02 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBBhPbQ9sPVAb2tVjzRSsbi3SmGA-59lHQ",
+    authDomain: "project-94-bf5ac.firebaseapp.com",
+    databaseURL: "https://project-94-bf5ac-default-rtdb.firebaseio.com",
+    projectId: "project-94-bf5ac",
+    storageBucket: "project-94-bf5ac.appspot.com",
+    messagingSenderId: "958914894328",
+    appId: "1:958914894328:web:7fc3a7825bf479804ede0d"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
